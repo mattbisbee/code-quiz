07eda5f3b4cc72fd3ebff5249d84dbf848fc3387
Biznaz <aldhelm7@gmail.com>
2021-05-16 20:02:59 -0700

    added html structure and some js

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+//displays where the user is at compared to the entire test, i.e, question 3 out of 4
+var testLocation = 0;
+
+//tracks correct answers
+var correctAnswers = 0;
+
+//contains the test div
+var test 
+
+//contains the playerProgress heading and displays the progress in the test
+var playerProgress 
+
+//will contain a question grabbed from an array
+var questions = [
+{
+  question: "Commonly used data types NO NOT include:",
+  a: "Strings",
+  b: "Booleans",
+  c: "Alerts",
+  d: "Numbers",
+  answer: "b"
+}
+];
+
+
+//contain the user's selected answer
+var selAnswer 
+
+// contains the possible answers to choose (A-D)
+var choices 
+
+
+// contains a possible answer
+var ansA 
+
+// contains a possible answer
+var ansB 
+
+// contains a possible answer
+var ansC 
+
+// contains a possible answer
+var ansD 
